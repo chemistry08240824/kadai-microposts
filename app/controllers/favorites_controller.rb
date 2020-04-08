f69a59be0d520fc273919e7751c263b36e1b85c2
kazumasa.nozawa <chemistry08240824@gmail.com>
2020-04-08 12:36:39 +0000

    modify5

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -5,14 +5,15 @@ class FavoritesController < ApplicationController
     micropost = Micropost.find(params[:micropost_id])
     current_user.like(micropost)
     flash[:success]='お気に入りに登録しました。'
-    redirect_to micropost.user
+    redirect_back(fallback_location: root_path)
+    
   end
 
   def destroy
     micropost = Micropost.find(params[:micropost_id])
     current_user.unlike(micropost)
     flash[:success]='お気に入りを解除しました。'
-    redirect_to micropost.user
+    redirect_back(fallback_location: root_path)
   end
   
 end
